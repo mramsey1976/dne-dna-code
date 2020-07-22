d443d5fdea64bc1f28ccb0f8e19f0cbd9a5de347
Mike Ramsey <mramsey76@icloud.com>
2020-07-22 09:45:03 -0400

    1

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -23,7 +23,7 @@ picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
 
 # TODO: Now call the function a few times to calculate the following answers
 
-print("12 x 96 =",)
+print(picked_fruit,)
 
 print("48 x 17 =",)
 
